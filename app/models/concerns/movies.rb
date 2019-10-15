30b64294667e1f6eee02828bbde773c56a73cd0a
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-10-14 19:46:56 -0500

    Powerfull API source implemented - TMDB

--- app/models/concerns/movies.rb
+++ app/models/concerns/movies.rb
@@ -2,32 +2,46 @@ module Movies
   module Api
     extend ActiveSupport::Concern
 
-    def find_by_title(key_word)
-      unless ENV["OMDB_API_KEY"]
+    #url = https://api.themoviedb.org/3/movie/552/recommendations?api_key=db9daceaaf741afe6a09c94d9c27cace
+
+    def multi_search(key_word)
+      unless ENV["TMDB_API_KEY"]
         raise ArgumentError, "FATAL ERROR, NO API KEY PROVIDED"
-      end      
-      url = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&t=#{key_word}"
+      end        
+      url = "https://api.themoviedb.org/3/search/multi?api_key="\
+        + ENV["TMDB_API_KEY"] + "&query=" + key_word + "&include_adult=false"
       response = HTTP.get(url).to_s
-      json_response = JSON.parse(response)            
+      JSON.parse(response)
+    rescue ArgumentError => e
+      json_response = { error: e }
     end
 
-    def find_by_id(id)
-      unless ENV["OMDB_API_KEY"]
-        raise ArgumentError, "FATAL ERROR, NO API KEY PROVIDED"
-      end      
-      url = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&i=#{id}"
-      response = HTTP.get(url).to_s
-      json_response = JSON.parse(response)      
-    end
+    # def find_by_title(key_word)
+    #   unless ENV["TMDB_API_KEY"]
+    #     raise ArgumentError, "FATAL ERROR, NO API KEY PROVIDED"
+    #   end      
+    #   url = "http://www.omdbapi.com/?apikey=#{ENV["TMDB_API_KEY"]}&t=#{key_word}"
+    #   response = HTTP.get(url).to_s
+    #   json_response = JSON.parse(response)            
+    # end
 
-    def search_by_title(key_word)
-      unless ENV["OMDB_API_KEY"]
-        raise ArgumentError, "FATAL ERROR, NO API KEY PROVIDED"
-      end
-      url = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&s=#{key_word}"
-      response = HTTP.get(url).to_s
-      puts response
-      json_response = JSON.parse(response)      
-    end      
+    # def find_by_id(id)
+    #   unless ENV["TMDB_API_KEY"]
+    #     raise ArgumentError, "FATAL ERROR, NO API KEY PROVIDED"
+    #   end      
+    #   url = "http://www.omdbapi.com/?apikey=#{ENV["TMDB_API_KEY"]}&i=#{id}"
+    #   response = HTTP.get(url).to_s
+    #   json_response = JSON.parse(response)      
+    # end
+
+    # def search_by_title(key_word)
+    #   unless ENV["TMDB_API_KEY"]
+    #     raise ArgumentError, "FATAL ERROR, NO API KEY PROVIDED"
+    #   end
+    #   url = "http://www.omdbapi.com/?apikey=#{ENV["TMDB_API_KEY"]}&s=#{key_word}"
+    #   response = HTTP.get(url).to_s
+    #   puts response
+    #   json_response = JSON.parse(response)      
+    # end      
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,14 +7,20 @@ class Movie < ApplicationRecord
     def search(key_word)
       movies = []
       unless key_word.empty?
-        api_movies = Movie.search_by_title(key_word)["Search"]
-        #puts api_movies.inspect
-        api_movies.each do |am|
-          movie = Movie.new
-          movie.imdb_id = am["imdbID"]
-          movie.title = am["Title"]
-          movie.img_url = am["Poster"]
-          movies << movie
+        api_movies = Movie.multi_search(key_word)["results"]
+        puts api_movies.inspect
+        api_movies.each do |movie_json|
+          if movie_json["title"] && movie_json["popularity"] >= 5
+            movie = Movie.new
+            movie.imdb_id = movie_json["id"]
+            movie.title = movie_json["title"]
+            if movie_json["poster_path"]
+              movie.img_url = "https://image.tmdb.org/t/p/original" + movie_json["poster_path"]
+            else
+              movie.img_url = "https://nerdbot.com/wp-content/uploads/2018/12/not_available.png"
+            end
+            movies << movie
+          end
         end
       end
       movies
